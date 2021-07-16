43500330083146bada5ffaf863ceb9e7cc07a73b
Tiago de Souza Silva <silvatiago.0407@gmail.com>
2021-07-16 18:47:02 -0300

    DIO

---
+++ Desafio Dio  Everis em .NET 🖥️.md
@@ -0,0 +1,3 @@
+## Desafio Dio / Everis em .NET 🖥️
+
+- Desenvolva um programa capaz de ler um valor inteiro N. N * 2 linhas de saída vão ser apresentadas na execução do programa, seguindo a lógica do exemplo mais abaixo. Para os valores com mais de seis dígitos, todos os dígitos devem ser apresentados.
---
+++ DesafioDio/DesafioDio/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DesafioDio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i + " " + (i * i) + " " + (i * i * i));
+                Console.WriteLine(i + " " + ((i * i) + 1) + " " + ((i * i * i) + 1));
+            }
+        }
+    }
+}
